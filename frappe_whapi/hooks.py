598b072572b9d15abc58b58e9a6da8c7687608fc
riekert7 <150682497+riekert7@users.noreply.github.com>
2025-03-11 12:25:52 +0200

    manually hooked function via hooks.py

--- frappe_whapi/hooks.py
+++ frappe_whapi/hooks.py
@@ -244,19 +244,25 @@ app_license = "mit"
 
 
 doc_events = {
-    "*": {
-        "before_insert": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "after_insert": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "before_validate": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "validate": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "on_update": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "before_submit": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "on_submit": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "before_cancel": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "on_cancel": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "on_trash": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "after_delete": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "before_update_after_submit": "frappe_whapi.utils.run_server_script_for_doc_event",
-        "on_update_after_submit": "frappe_whapi.utils.run_server_script_for_doc_event"
+    "Whapi Message": {
+        "before_insert": "frappe_whapi.frappe_whapi.doctype.whapi_message.whapi_message.before_insert"
     }
 }
+
+# doc_events = {
+#     "*": {
+#         "before_insert": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "after_insert": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "before_validate": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "validate": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "on_update": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "before_submit": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "on_submit": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "before_cancel": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "on_cancel": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "on_trash": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "after_delete": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "before_update_after_submit": "frappe_whapi.utils.run_server_script_for_doc_event",
+#         "on_update_after_submit": "frappe_whapi.utils.run_server_script_for_doc_event"
+#     }
+# }
